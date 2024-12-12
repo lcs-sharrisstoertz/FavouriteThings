5e2a559aaae5547fc15f719b05e4e149b51ce791
Skye Harris-Stoertz <sharrisstoertz@lcs.on.ca>
2024-12-11 21:44:16 -0500

    Added toolbar and navigation title

---
+++ FavouriteThings/Views/FavouriteThingsView.swift
@@ -0,0 +1,24 @@
+//
+//  FavouriteThingsView.swift
+//  FavouriteThings
+//
+//  Created by Skye Willow Harris-Stoertz on 2024-12-11.
+//
+
+import SwiftUI
+
+struct FavouriteThingsView: View {
+    var body: some View {
+        NavigationStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("My Favourite Things")
+                .toolbar {
+                }
+        }
+    }
+}
+    
+
+#Preview {
+    FavouriteThingsView()
+}
