69e52f1c2d7c3b73b49e4f8cf76af95c228da692
Skye Harris-Stoertz <sharrisstoertz@lcs.on.ca>
2024-12-11 21:30:51 -0500

    Initial Commit

---
+++ FavouriteThings/FavouriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteThingsApp.swift
+//  FavouriteThings
+//
+//  Created by Skye Willow Harris-Stoertz on 2024-12-11.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
